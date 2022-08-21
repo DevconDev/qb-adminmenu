c869e3016e1ae50881fc50f0c788955da3fb9802
DevconDev <devon.hopkins333@gmail.com>
2022-08-21 09:18:32 -0300

    added bring command

--- server/server.lua
+++ server/server.lua
@@ -474,3 +474,24 @@ CreateThread(function()
         Wait(1500)
     end
 end)
+local savedCoords = {}
+
+QBCore.Commands.Add('bring', 'Bring a player to you (Admin only)', { { name = 'id', help = 'Player ID' }, }, true, function(source, args)
+    if args[1] then
+        local src = source
+        local admin = GetPlayerPed(src)
+        local coords = GetEntityCoords(admin) --Admin coords
+        local target = GetPlayerPed(tonumber(args[1])) --Player ped
+        SetEntityCoords(target, coords)
+        savedCoords[tonumber(args[1])]= GetEntityCoords(target) --Save player coords
+    end
+end, 'admin')
+
+QBCore.Commands.Add('bringback', 'Bring back a player (Admin only)', { { name = 'id', help = 'Player ID' }, }, true, function(source, args)
+    if args[1] then
+        local src = source
+        local coords = savedCoords[tonumber(args[1])] --Player saved coords
+        local target = GetPlayerPed(tonumber(args[1])) --Player ped
+        SetEntityCoords(target, coords)
+    end
+end, 'admin')
